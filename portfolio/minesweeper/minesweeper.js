3432530e9503471dee76ff65e65fccb7b1eaf713
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2022-07-22 12:30:22 -0700

    Mineweeper development

--- portfolio/minesweeper/minesweeper.js
+++ portfolio/minesweeper/minesweeper.js
@@ -3,6 +3,7 @@ function Cell(e, row, col){
     this.row = row;
     this.col = col;
     this.mine = false;
+    this.nearbyMineCount = 0;
 
     this.setMine = function() {
         this.mine = true;
@@ -14,13 +15,13 @@ const board = {
     width: 16,
     height: 16,
     cellCount: 16*16,
-    bombCount: 16,
+    mineCount: 16,
     cells: [],
 
-    createBoard: function(width, height, bombCount) {
+    createBoard: function(width, height, mineCount) {
         this.width = width;
         this.height = height;
-        this.bombCount = bombCount
+        this.mineCount = mineCount
         this.cellCount = this.width * this.height;
 
         for(let i = 0; i < this.width; i++){
@@ -36,16 +37,68 @@ const board = {
         };
     },
 
+    createMines: function() {
+        let row = Math.floor(Math.random()*16);
+        let col = Math.floor(Math.random()*16);
+        if(!this.cells[row][col].mine){
+            this.cells[row][col].mine = true;
+        }else{
+            this.createMines();
+        }
+    },
+
     populate: function() {
-        for(let i = 0; i < this.bombCount; i++){
-            this.cells[Math.floor(Math.random()*16)][Math.floor(Math.random()*16)].setMine()
-        };
+        console.log('pop');
+        for(let i = 0; i < this.mineCount; i++){
+            this.createMines();
+        }
+        for(let row = 0; row < this.width; row++){
+            for(let col  = 0; col < this.height; col++){
+                for(let i = -1; i <= 1; i++){
+                    for(let j = -1; j <= 1; j++){
+                        if(row+i >= 0 && row+i < this.height && col+j >= 0 && col+j < this.width){
+                            if(this.cells[row+i][col+j].mine){
+                                this.cells[row][col].nearbyMineCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    printBoard: function() {
+        out = '';
+        for(let i = 0; i < this.width; i++){
+            row = '';
+            for(let j = 0; j < this.height; j++){
+                if(this.cells[i][j].mine){
+                    row += '# ';
+                }else if(this.cells[i][j].nearbyMineCount > 0){
+                    row += this.cells[i][j].nearbyMineCount + ' ';
+                }else{
+                    row += '. ';
+                }
+            }
+            out += `
+            ` + row;
+        }
+        console.log(out);
+    },
+
+    search: function(row, col) {
+        if(this.cells[row][col].mine){
+            this.e.classList.add('explode');
+            return false;
+        }else{
+            let nearbyMineCount = 0
+            this.e.classList.add('searched');
+            return true;
+        }
     }
 };
 
 board.createBoard(16,16,16);
-
-console.log(board.cellCount);
-
-
+board.populate();
+board.printBoard();
 console.log('JAVASCRIPT LOADED')
