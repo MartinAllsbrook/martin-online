0da5166b4617b1b72e31daa6e0c77439d1dfb71a
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2022-08-30 11:53:11 -0600

    Welcome page added :)

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let linkedfrom = 'homepage';
 sessionStorage.setItem('linkedfrom', linkedfrom);
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('enter').addEventListener('click', () => {
+        // document.getElementById('welcome').style.opacity = '0';
+        document.getElementById('welcome').style.display  = 'none';
+    }, false)
+}, false)
