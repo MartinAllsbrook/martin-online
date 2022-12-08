be726e86bb20e5280df9e937683c6bb7f87391de
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2022-12-08 09:47:39 -0700

    Basic coloring
    added some basic coloring to cards
    Suit letter and face card letters are displayed as will

--- web-class-portal/solitaire/src/PlayingCard.js
+++ web-class-portal/solitaire/src/PlayingCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import SolitaireBoard from "./SolitaireBoard";
 
 export default class PlayingCard extends React.Component {
     constructor(props) {
@@ -35,6 +34,26 @@ export default class PlayingCard extends React.Component {
     }
 
     render() {
+        let suit;
+        if(this.state.suit === 1){
+            suit = 'S';
+        }else if(this.state.suit === 2){
+            suit = 'H';
+        }else if(this.state.suit === 3){
+            suit = 'C';
+        }else if(this.state.suit === 4){
+            suit = 'D';
+        }
+
+        let rank = this.state.rank;
+        if(rank === 13){
+            rank = "K";
+        }else if(rank === 12){
+            rank = "Q";
+        }else if(rank === 11){
+            rank = "J";
+        }
+
         // console.log("card update: " + this.state.hidden);
         if (this.state.hidden){
             return (
@@ -46,20 +65,45 @@ export default class PlayingCard extends React.Component {
             )
         }
         if (this.state.selected){
+            if(suit === 'S' || suit === 'C'){
+                return (
+                    <div class="playing-card black selected" onClick={this.onCardSelect}>
+                        <p> {suit} </p>
+                        <p> , </p>
+                        <p> {rank} </p>
+                    </div>
+                )
+            }else if(suit === 'H' || suit === 'D'){
+                return (
+                    <div class="playing-card red selected" onClick={this.onCardSelect}>
+                        <p> {suit} </p>
+                        <p> , </p>
+                        <p> {rank} </p>
+                    </div>
+                )
+            }
+        }
+
+        if(suit === 'S' || suit === 'C'){
             return (
-                <div class="playing-card selected" onClick={this.onCardSelect}>
-                    <p> {this.state.suit} </p>
-                    <p>,</p>
-                    <p> {this.state.rank} </p>
+                <div class="playing-card black" onClick={this.onCardSelect}>
+                    <p> {suit} </p>
+                    <p> , </p>
+                    <p> {rank} </p>
+                </div>
+            )
+        }else if(suit === 'H' || suit === 'D'){
+            return (
+                <div class="playing-card red" onClick={this.onCardSelect}>
+                    <p> {suit} </p>
+                    <p> , </p>
+                    <p> {rank} </p>
                 </div>
             )
         }
-
         return (
             <div class="playing-card" onClick={this.onCardSelect}>
-                <p> {this.state.suit} </p>
-                <p>,</p>
-                <p> {this.state.rank} </p>
+                <p> Blank </p>
             </div>
         )
     }
