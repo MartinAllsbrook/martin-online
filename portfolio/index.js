02f1ddc38dc4f75bfc75eb80d9dfbe036d908119
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-05-17 02:13:14 -0600

    Push to laptop

--- portfolio/index.js
+++ portfolio/index.js
@@ -27,8 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let titles = ['SNAKE', 'GLEE 2023', 'GROWAPP', 'TYPE SPECIMEN POSTER', 'HANDBOARD GAME POC', 'COLORADO SYMPHONY', 'BIG WORMS'];
     let images = ['snake.svg', 'glee-2023.svg', 'growapp.svg', 'type-specimen.svg', 'handboard-poc.svg', 'colorado-symphony.svg', 'big-worms.svg']
 
-    items.push(new Item('snake', 'SNAKE', 'snake.svg'));
+    items.push(new Item('xenoplanet', 'XENOPLANET', 'xenoplanet.png'));
     items.push(new Item('https://youtu.be/opKY8cLKrIY', 'THE GUILLOTINE', 'the-guillotine.png'));
+    items.push(new Item('snake', 'SNAKE', 'snake.svg'));
     items.push(new Item('glee-2023', 'GLEE 2023', 'glee-2023.svg'));
     items.push(new Item('growapp', 'GROWAPP', 'growapp.svg'));
     items.push(new Item('type-specimen', 'TYPE SPECIMEN POSTER', 'type-specimen.svg'));
