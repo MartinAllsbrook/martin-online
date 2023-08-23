6adcfe2e07346af6c3776c935a97a7ac5d88cc68
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-08-23 16:59:17 -0700

    mobile friendly sizing and new thumbnails
    + Website (homepage and projects) visible at more scales
    + Some new thumbnails

--- portfolio/index.js
+++ portfolio/index.js
@@ -27,13 +27,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let titles = ['SNAKE', 'GLEE 2023', 'GROWAPP', 'TYPE SPECIMEN POSTER', 'HANDBOARD GAME POC', 'COLORADO SYMPHONY', 'BIG WORMS'];
     let images = ['snake.svg', 'glee-2023.svg', 'growapp.svg', 'type-specimen.svg', 'handboard-poc.svg', 'colorado-symphony.svg', 'big-worms.svg']
 
-    items.push(new Item('xenoplanet', 'XENOPLANET', 'xenoplanet.png'));
+    items.push(new Item('xenoplanet', 'XENOPLANET', 'xenoplanet2.png'));
     items.push(new Item('guillotine', 'THE GUILLOTINE', 'the-guillotine.png'));
     items.push(new Item('../arcade/snake-legacy', 'SNAKE', 'snake.svg'));
-    items.push(new Item('glee-2023', 'GLEE 2023', 'glee-2023.svg'));
+    items.push(new Item('glee-2023', 'GLEE 2023', 'glee 2.png'));
     items.push(new Item('growapp', 'GROWAPP', 'growapp.svg'));
     items.push(new Item('type-specimen', 'TYPE SPECIMEN POSTER', 'type-specimen.svg'));
-    items.push(new Item('handboard-poc', 'HANDBOARD GAME', 'handboard-poc.svg'));
+    items.push(new Item('handboard-poc', 'HANDBOARD GAME', 'handboard-poc2.png'));
     items.push(new Item('colorado-symphony', 'COLORADO SYMPHONY', 'colorado-symphony.svg'));
     items.push(new Item('big-worms', 'BIG WORMS', 'big-worms.svg'));
 
